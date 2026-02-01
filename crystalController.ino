ce799e254614aafa4953d06be62ca0adf0b403dc
Jonathan Page <me@jonnypage.ca>
2026-02-01 14:21:18 -0700

    chore: :pencil2: updated arduino sketch

--- crystalController.ino
+++ crystalController.ino
@@ -3,9 +3,9 @@
 #include <U8g2lib.h>
 
 // ===== CONFIG =====
-const char *WIFI_SSID = "YOUR_WIFI";
-const char *WIFI_PASS = "YOUR_PASS";
-const char *SERVER_URL = "http://192.168.8.142:3000/event"; // Pi IP - change if different
+const char* WIFI_SSID = "FF2026"; 
+const char* WIFI_PASS = "1111100000"; 
+const char* SERVER_URL = "http://192.168.8.142:3000/event"; // Pi endpoint 
 
 // Safe GPIOs for 7 switches (avoid 0,5,6,8)
 const int switchPins[7] = {1, 2, 3, 4, 7, 9, 10};
@@ -14,7 +14,7 @@ U8G2_SSD1306_72X40_ER_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, 6, 5);
 
 bool slotState[7] = {false};     // current physical state
 bool lastSlotState[7] = {false}; // previous loop state
-bool pendingSend[7] = {false};
+bool pendingStateSync = false;   // send full state on next retry
 
 int crystalCount = 0;
 bool wifiConnected = false;
@@ -47,8 +47,8 @@ void loop()
   {
     slotState[i] = (digitalRead(switchPins[i]) == LOW); // pressed = LOW
 
-    if (slotState[i] && !lastSlotState[i])
-      pendingSend[i] = true; // send only on press
+    if (slotState[i] != lastSlotState[i])
+      pendingStateSync = true; // any change: send full state
 
     lastSlotState[i] = slotState[i];
 
@@ -78,24 +78,28 @@ void checkWifi()
 // ===== SEND EVENTS =====
 void retryPending()
 {
-  for (int i = 0; i < 7; i++)
-  {
-    if (pendingSend[i])
-    {
-      if (sendCrystalEvent(i))
-        pendingSend[i] = false;
-    }
-  }
+  if (!pendingStateSync)
+    return;
+  if (sendCrystalEvent())
+    pendingStateSync = false;
 }
 
-bool sendCrystalEvent(int slot)
+bool sendCrystalEvent()
 {
   HTTPClient http;
   http.begin(SERVER_URL);
   http.addHeader("Content-Type", "application/json");
   http.setTimeout(5000);
 
-  String payload = "{ \"type\": \"crystal\", \"slot\": " + String(slot) + " }";
+  // Full state: type, slots array [true/false...], count
+  String slotsJson = "[";
+  for (int i = 0; i < 7; i++)
+  {
+    if (i > 0) slotsJson += ",";
+    slotsJson += slotState[i] ? "true" : "false";
+  }
+  slotsJson += "]";
+  String payload = "{ \"type\": \"crystal\", \"slots\": " + slotsJson + ", \"count\": " + String(crystalCount) + " }";
   int code = http.POST(payload);
   http.end();
 
@@ -106,7 +110,8 @@ bool sendCrystalEvent(int slot)
   }
   else
   {
-    drawStatus("Err " + String(code), crystalCount);
+    String errMsg = "Err " + String(code);
+    drawStatus(errMsg.c_str(), crystalCount);
   }
   return (code == 200);
 }
